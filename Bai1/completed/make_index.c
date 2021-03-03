91c20b74610b164b0ae3df734550c310eca8afb7
Sherly1001 <Sherly1001@users.noreply.github.com>
2021-03-03 17:13:53 +0700

    w1

---
+++ Bai1/completed/make_index.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct array {
+    int row, col;
+    struct array *next;
+} array;
+
+int add_val(array **arr, int row, int col) {
+    if (*arr) {
+        array *new = malloc(sizeof(array));
+        new->row = row;
+        new->col = col;
+        new->next = *arr;
+        *arr = new;
+    } else {
+        *arr = malloc(sizeof(array));
+        (*arr)->row = row;
+        (*arr)->col = col;
+        (*arr)->next = NULL;
+    }
+}
+
+typedef struct word_tree {
+    char *word;
+    array *arr;
+    struct word_tree *left;
+    struct word_tree *right;
+} word_tree;
+
+char *add_word(word_tree **wl, const char *word, int row, int col) {
+    if (*wl) {
+        int cmp = strcmp((*wl)->word, word);
+        if (cmp == 0) add_val(&(*wl)->arr, row, col);
+        else if (cmp > 0) add_word(&(*wl)->left, word, row, col);
+        else add_word(&(*wl)->right, word, row, col);
+    } else {
+        *wl = malloc(sizeof(word_tree*));
+        (*wl)->word = malloc(sizeof(char) * 20);
+        strcpy((*wl)->word, word);
+        (*wl)->arr = NULL;
+        add_val(&(*wl)->arr, row, col);
+        (*wl)->left = NULL;
+        (*wl)->right = NULL;
+    }
+}
+
+typedef struct word_list {
+    char *word;
+    struct word_list *next;
+} word_list;
+
+char *add_stopw(word_list **arr, const char *word) {
+    if (*arr) {
+        word_list *new = malloc(sizeof(word_list));
+        new->word = malloc(sizeof(char) * 20);
+        strcpy(new->word, word);
+        new->next = *arr;
+        *arr = new;
+    } else {
+        *arr = malloc(sizeof(word_list));
+        (*arr)->word = malloc(sizeof(char) * 20);
+        strcpy((*arr)->word, word);
+        (*arr)->next = NULL;
+    }
+}
+
+word_list *load_word_list(const char *filename) {
+    FILE *f = fopen(filename, "r");
+    char w[20];
+    word_list *stopw = NULL;
+    while (fscanf(f, " %[^\n]s\n", w) > 0) add_stopw(&stopw, w);
+    fclose(f);
+    return stopw;
+}
+
+word_tree *load_word_tree(const char *filename, word_list *stopw, word_list *mean_name) {
+    FILE *f = fopen(filename, "r");
+    char s[200];
+    int row = 1;
+    word_tree *wt = NULL;
+
+    while (fgets(s, 200, f)) {
+        int i = 0, j = 0;
+        while (s[i]) {
+            if (isalpha(s[i])) {
+                j = i + 1;
+                while (isalpha(s[j]) || s[j] == '.') ++j;
+                
+                char tmp = s[j], word[20];
+                s[j] = '\0';
+                strcpy(word, s + i);
+                s[j] = tmp;
+
+                int word_type = 0; // 0 => none, 1 => mean_name, 2 => stopword
+
+                for (word_list *w = mean_name; w; w = w->next) {
+                    if (!strcmp(w->word, word)) {
+                        add_word(&wt, w->word, row, i + 1);
+                        word_type = 1;
+                        break;
+                    }
+                }
+
+                for (char *w = word; *w; ++w) {
+                    *w = tolower(*w);
+                }
+                if (word[j - i - 1] == '.') word[j - i - 1] = '\0';
+
+                if (word_type != 1) {
+                    for (word_list *w = stopw; w; w = w->next) {
+                        int is_same = 1;
+                        for (int i = 0; word[i]; ++i) {
+                            if (word[i] != w->word[i]) {
+                                is_same = 0;
+                                break;
+                            }
+                        }
+                        if (is_same) {
+                            word_type = 2;
+                        }
+                    }
+                }
+
+                if (word_type == 0) add_word(&wt, word, row, i + 1);
+                
+                i = j;
+            } else {
+                ++i;
+            }
+        }
+        ++row;
+    }
+
+    fclose(f);
+    return wt;
+}
+
+void print_index(array *arr, int count) {
+    if (arr) {
+        print_index(arr->next, count + 1);
+        printf("(%d, %d) ", arr->row, arr->col);
+    } else {
+        printf("%d, ", count);
+    }
+}
+
+void show_index(word_tree *index) {
+    if (index) {
+        show_index(index->left);
+        printf("%s ", index->word);
+        print_index(index->arr, 0);
+        printf("\n");
+        show_index(index->right);
+    }
+}
+
+int main() {
+    word_list *stopw = load_word_list("stopw.txt");
+    word_list *mean_name = load_word_list("mean_name.txt");
+    word_tree *index = load_word_tree("vanban.txt", stopw, mean_name);
+    show_index(index);
+}
